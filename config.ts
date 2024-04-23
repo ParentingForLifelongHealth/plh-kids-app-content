ce8403a606fcf8b716543a814b54c334aa8fb714
EsmeeIDEMS <esmee@idems.international>
2024-04-23 14:02:11 +0100

    content: 1.1.7

--- config.ts
+++ config.ts
@@ -15,7 +15,7 @@ config.android = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/plh-kids-app-content.git",
-  content_tag_latest: "1.1.6",
+  content_tag_latest: "1.1.7",
 };
 
 config.api.db_name = "plh_kids"
